d18b6579941761fc74e2b454703f09c01b8524a0
IliaGavrilov <IliaGavrilov@ILIAGAVRILOV-HP>
2021-10-20 21:44:11 +0300

    Добавьте файлы проекта.

---
+++ Lab6-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            String[] stringArray = startString.Split();
+            int a = 0;
+            string str ="";
+            foreach (string s in stringArray)
+            {
+                if (a < s.Length)
+                { a = s.Length;
+                    str = s;
+                } 
+                                        
+            }
+            Console.Write(str);
+            Console.ReadKey();
+        }
+    }
+}
